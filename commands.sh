47cd362d54c3432c864c5a026c72173ee15b3cc2
Ram Vegiraju <48113975+RamVegiraju@users.noreply.github.com>
2022-07-18 23:23:57 -0400

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -3,3 +3,6 @@ docker exec -it <container ID> ls -la
 
 # Shell prompt within container
 docker run -it <image-name> sh
+
+# Run Docker container on port
+docker run -p <local host port>:<container-port> ramveg/ml-app
