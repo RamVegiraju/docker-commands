ed41d8ee720308be592a5815329cef86718bc8b9
Ram Vegiraju <48113975+RamVegiraju@users.noreply.github.com>
2022-07-21 15:51:57 -0400

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -6,3 +6,6 @@ docker run -it <image-name> sh
 
 # Run Docker container on port
 docker run -p <local host port>:<container-port> <container-name>
+
+# Building custom Dockerfiles
+docker build -f Dockerfile.dev .
